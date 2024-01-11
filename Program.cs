e2d7a6c2a46681cb4c7bc21544d14b86b01e0031
Mertcan Taşkıran <65121494+mertcan-taskiran@users.noreply.github.com>
2024-01-11 16:17:44 +0300

    many to many

--- Program.cs
+++ Program.cs
@@ -9,6 +9,8 @@ public class ShopContext: DbContext
     public DbSet<User> Users {get;set;}
     public DbSet<Address> Addresses {get;set;}
     public DbSet<Customer> Customers {get;set;}
+    public DbSet<Product> Products {get;set;}
+    public DbSet<Category> Categories {get;set;}
 
     public static readonly ILoggerFactory MyLoggerFactory = LoggerFactory.Create(builder => { builder.AddConsole(); }); // LINQ to SQL at Console
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
@@ -16,6 +18,22 @@ public class ShopContext: DbContext
         optionsBuilder
         .UseLoggerFactory(MyLoggerFactory)
         .UseSqlite("Data Source=shop2.db");
+    }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ProductCategory>()
+            .HasKey(t => new {t.ProductId ,t.CategoryId});
+
+        modelBuilder.Entity<ProductCategory>()
+            .HasOne(pc => pc.Product)
+            .WithMany(p=>p.ProductCategories)
+            .HasForeignKey(pc => pc.ProductId);
+
+        modelBuilder.Entity<ProductCategory>()
+            .HasOne(pc => pc.Category)
+            .WithMany(c=>c.ProductCategories)
+            .HasForeignKey(pc => pc.CategoryId);
     }
 }
 
@@ -58,6 +76,29 @@ public class Supplier
     public int Id { get; set; } // Primary Key
     public string Name { get; set; }
     public string TaxNumber { get; set; }
+}
+
+public class Product
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public decimal Price { get; set; }
+    public List<ProductCategory> ProductCategories {get; set;} // Many to many
+}
+
+public class Category
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public List<ProductCategory> ProductCategories {get; set;} // Many to many
+}
+
+public class ProductCategory
+{
+    public int ProductId { get; set; }
+    public Product Product { get; set; }
+    public int CategoryId { get; set; }
+    public Category Category { get; set; }
 }
 
 class Program
@@ -132,6 +173,31 @@ class Program
     
     static void Main()
     {
-        InsertUsers();
+        // using (var db = new ShopContext())
+        // {
+        //     var products = new List<Product>
+        //     {
+        //         new Product(){Name="IPhone 14", Price=6000},
+        //         new Product(){Name="IPhone 13", Price=4000},
+        //         new Product(){Name="Monster Notebook", Price=5000},
+        //     };
+        //     // db.Products.AddRange(products);
+
+        //     var categories = new List<Category>
+        //     {
+        //         new Category(){Name="Telefon"},
+        //         new Category(){Name="Bilgisayar"},
+        //         new Category(){Name="Elektronik"},
+        //     };
+        //     db.Categories.AddRange(categories);
+            
+        //     int[] ids = new int[2]{1,2};
+        //     var p = db.Products.Find(1);
+        //     p.ProductCategories = ids.Select(cid=> new ProductCategory(){
+        //         CategoryId = cid,
+        //         ProductId = p.Id
+        //     }).ToList();
+        //     db.SaveChanges();
+        // }
     }
 }
--- obj/Debug/net8.0/EfCore2.AssemblyInfo.cs
+++ obj/Debug/net8.0/EfCore2.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EfCore2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1822825e8d9a3a790d26b298e2af5d83c25f3eab")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+578028ddb53a7bfdce47bd7ee09028ee2de823a3")]
 [assembly: System.Reflection.AssemblyProductAttribute("EfCore2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EfCore2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
